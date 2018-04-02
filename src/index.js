ce47e3b7895586a45100c5deae0c749e607351de
qiuhaoxin <502967244@qq.com>
2018-04-02 11:45:08 +0800

    commit

---
+++ src/index.js
@@ -0,0 +1,12 @@
+import React,{Component} from 'react';
+import PropTypes from 'prop-types';
+import {Provider} from 'react-redux';
+import {render} from 'react-dom';
+import {createStore,applyMiddleware} from 'redux';
+
+
+
+render({
+
+});
+
